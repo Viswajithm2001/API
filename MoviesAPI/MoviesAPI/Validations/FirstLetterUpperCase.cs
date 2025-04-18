66152456593e3405e22f890cf1cb9137eb65330e
Viswajithm2001 <viswajithm2001@gmail.com>
2025-04-18 20:25:25 +0530

    Commit the validation check

---
+++ MoviesAPI/MoviesAPI/Validations/FirstLetterUpperCase.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MoviesAPI.Validations
+{
+    public class FirstLetterUpperCaseAttribute: ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if(value == null|| string.IsNullOrEmpty(value.ToString()))
+            {
+                return new ValidationResult("The field cannot be empty");
+            }
+            var firstLetter = value.ToString()[0];
+            if(char.IsUpper(firstLetter))
+            {
+                return ValidationResult.Success;
+            }
+            else
+            {
+                return new ValidationResult("The first letter must be uppercase");
+            }
+        }
+    }
+}
